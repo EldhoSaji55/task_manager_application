55a1bacc43003d12c138ece19f33f0456a20d33a
EldhoSaji55 <eldhosaji4755@gmail.com>
2024-11-09 23:22:45 +0530

    colorupdate

--- lib/utils/constants/color_constants.dart
+++ lib/utils/constants/color_constants.dart
@@ -7,4 +7,12 @@ class ColorConstants {
   static const Color mainBlack = Colors.black;
   static const Color mainWhite = Colors.white;
   static const Color Maingrey = Colors.grey;
+  static const List<Color> calenderColorList = [
+    Color(0xFF4A90E2), // Bright Blue
+    Color(0xFFFF6F61), // Coral Red
+    Color(0xFF7ED321), // Bright Green
+    Color(0xFF9B59B6), // Lavender Purple
+    Color(0xFFFFB300), // Golden Yellow
+    Color(0xFFF06292), // Hot Pink// Blush Pink
+  ];
 }
--- lib/view/Calender_Screen/calender_screen.dart
+++ lib/view/Calender_Screen/calender_screen.dart
@@ -1,40 +1,108 @@
+import 'package:easy_date_timeline/easy_date_timeline.dart';
 import 'package:flutter/material.dart';
 import 'package:task_manager_application/utils/constants/color_constants.dart';
 
-class CalenderScreen extends StatelessWidget {
+class CalenderScreen extends StatefulWidget {
   const CalenderScreen({super.key});
 
+  @override
+  State<CalenderScreen> createState() => _CalenderScreenState();
+}
+
+class _CalenderScreenState extends State<CalenderScreen> {
+  DateTime? selectedDateUpdate;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: _AppbarSection(),
-        body: Column(
-          children: [
-            _BodyDateAsTitleSection(),
-          ],
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              horizontalScrollCalender(),
+              SizedBox(
+                height: 20,
+              ),
+              _ScheduleSection()
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget _BodyDateAsTitleSection() {
+  Widget _ScheduleSection() {
     return Padding(
-      padding: const EdgeInsets.all(20.0),
+      padding: const EdgeInsets.all(12.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            "Today",
-            style: TextStyle(
-                fontSize: 18,
-                color: ColorConstants.Maingrey,
-                fontWeight: FontWeight.w500),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Schedule",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              Container(
+                height: 35,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor:
+                            WidgetStatePropertyAll(ColorConstants.primaryColor),
+                        foregroundColor:
+                            WidgetStatePropertyAll(ColorConstants.mainWhite),
+                        shape: WidgetStatePropertyAll(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)))),
+                    child: Text("+ Add New")),
+              )
+            ],
           ),
-          Text(
-            "Monday, 04 Apr",
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          SizedBox(
+            height: 10,
           ),
+          SizedBox(
+            height: 10 * 85,
+            child: ListView.separated(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) => ListTile(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      tileColor: ColorConstants.calenderColorList[
+                              index % ColorConstants.calenderColorList.length]
+                          .withOpacity(0.25),
+                      leading: Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                            color: ColorConstants.calenderColorList[index %
+                                ColorConstants.calenderColorList.length],
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.network(
+                              "https://img.freepik.com/free-vector/tiny-programmers-big-laptop-writing-script-coders-app-developers-with-computers-flat-vector-illustration-programming-engineering-software-development-concept-banner-landing-web-page_74855-25360.jpg?t=st=1731171984~exp=1731175584~hmac=26dc6decbe90b6fb9364c486069571dfd3f252005f337488189f48e0b6dd6990&w=1060"),
+                        ),
+                      ),
+                      title: Text(
+                        "UI Design Presentation",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        "09:00 AM - 11:00 AM",
+                        style: TextStyle(
+                            color: ColorConstants.calenderColorList[index %
+                                ColorConstants.calenderColorList.length],
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                separatorBuilder: (context, index) => SizedBox(
+                      height: 10,
+                    ),
+                itemCount: 10),
+          )
         ],
       ),
     );
@@ -47,6 +115,42 @@ class CalenderScreen extends StatelessWidget {
         "Upcoming Task",
         style: TextStyle(fontWeight: FontWeight.bold),
       )),
+    );
+  }
+}
+
+class horizontalScrollCalender extends StatelessWidget {
+  const horizontalScrollCalender({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return EasyDateTimeLine(
+      initialDate: DateTime.now(),
+      onDateChange: (selectedDate) {
+        // selectedDateUpdate = selectedDate;
+      },
+      headerProps: const EasyHeaderProps(
+        monthPickerType: MonthPickerType.switcher,
+        dateFormatter: DateFormatter.custom("EEEE \ndd MMMM yyyy"),
+      ),
+      dayProps: const EasyDayProps(
+        dayStructure: DayStructure.dayStrDayNum,
+        activeDayStyle: DayStyle(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                ColorConstants.secondaryColor,
+                ColorConstants.primaryColor,
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
